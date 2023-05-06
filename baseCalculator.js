11fe75192158fd049e73d7b72fce611301586d83
HugoMany <90179566+HugoMany@users.noreply.github.com>
2023-05-06 15:53:43 +0200

    Some Fixes & comment

--- baseCalculator.js
+++ baseCalculator.js
@@ -1,89 +1,77 @@
 class BaseCalculator {
-  constructor() {
-    this.liste = [];
-    this.prec = [];
-    this.allNumber = "";
-    this.start, this.end, this.time;
-    this.timeIcon = "<i class='fa-regular fa-hourglass'></i>";
-  }
-
-  // Ajoute l'input à la liste et à l'écran
-  addToList(arg) {
-    // Dès que l'utilisateur commence à écrire, on commence à mesurer
-    if (this.liste.length == 0) {
-      this.start = performance.now();
+    constructor() {
+        this.liste = []; // une liste d'opérandes et d'opérateurs pour l'opération en cours
+        this.prec = []; // la liste d'opérandes et d'opérateurs de l'opération précédente
+        this.allNumber = ""; // la chaîne de caractères qui contient l'opération en cours, sans espaces
+        this.start, this.end, this.time; // les variables pour mesurer le temps d'opération
+        this.timeIcon = "<i class='fa-regular fa-hourglass'></i>"; // une icône pour indiquer le temps d'opération à l'utilisateur
     }
-    // Si l'input n'est pas un chiffre
-    if (!isFinite(arg)) {
-      this.liste.push(" " + arg + " "); // " " est notre séparateur
-    } else {
-      this.liste.push(arg); // juste notre chiffre
+
+    // Ajoute l'input à la liste et à l'écran
+    addToList(arg) {
+        if (this.liste.length == 0) { // si la liste est vide, on commence à mesurer le temps
+            this.start = performance.now();
+        }
+        if (!isFinite(arg)) { // si l'argument n'est pas un chiffre
+            this.liste.push(" " + arg + " "); // on ajoute l'argument à la liste, avec des espaces autour pour séparer les opérateurs
+        } else {
+            this.liste.push(arg); // sinon, on ajoute juste l'argument à la liste
+        }
+        document.getElementById("result").innerHTML = this.liste.join(""); // on affiche l'opération à l'écran, sans les espaces autour des opérateurs
     }
-    document.getElementById("result").innerHTML = this.liste.join(""); // Affiche l'opération à l'écran
-  }
 
-  // Vérifie que notre syntaxe est correcte e.g. pas 1..1 / 2
-  secureEval(expression) {
-    try {
-      return eval(expression);
-    } catch (error) {
-      return new Error("Syntaxe");
+    // Vérifie que notre syntaxe est correcte e.g. pas 1..1 / 2
+    secureEval(expression) {
+        try {
+            return eval(expression); // on utilise la fonction eval() pour évaluer l'expression
+        } catch (error) {
+            return new Error("Syntaxe"); // si on attrape une erreur, on renvoie une erreur "Syntaxe"
+        }
     }
-  }
 
-  joinList() {
-    this.allNumber = this.liste.join("");
-    this.allNumber = this.allNumber.replace(/\s+/g, ""); // Retire les espaces induits par nos séparateurs (opérateurs)
-  }
-
-  calc() {
-    // On traite notre chaîne pour la rendre lisible par eval()
-
-    this.joinList();
-    console.log(this.liste);
-    console.log(this.allNumber);
-
-    document.getElementById("result").innerHTML = this.secureEval(
-      this.allNumber
-    ); // Affiche le résultat
-
-    document.getElementById("calcul").innerHTML = this.allNumber; // Affiche le calcul qu'on vient de faire
-
-    this.prec = []; // On supprime l'opération précédente
-    this.prec = this.liste; // On sauvegarde notre opération qui devient la précédente
-    this.liste = []; // On vide la liste pour pouvoir faire un nouveau calcul
-
-    // L'opération est terminée, on peut calculer le temps qu'elle a prit
-    this.end = performance.now();
-    this.time = (this.end - this.start) / 1000;
-    console.log(`Temps d'opération: ${this.time}s`);
-    document.title = `Temps d'opération: ${this.time}s`;
-    document.getElementById("time").innerHTML =
-      this.timeIcon + "\t" + this.time + "s";
-  }
-
-  // Supprime l'input qui vient d'être fait
-  del() {
-    if (document.getElementById("result").innerHTML == "") {
-      // Si jamais on appuie deux fois sur del après avoir fini un calcul
-      document.getElementById("result").innerHTML =
-        document.getElementById("calcul").innerHTML; // Notre résultat précédent devient le courant
-      document.getElementById("calcul").innerHTML = ""; // On efface ce qu'on avait avant
-      this.liste = this.prec; // On remplace la liste des arguments
-      this.start = performance.now(); // C'est un nouveau calcul, il ne faut pas oublier de reset le timer
-    } else {
-      // Sinon, on supprime juste le dernier caractère
-      this.liste.pop();
-      document.getElementById("result").innerHTML = this.liste.join("");
+    joinList() { // assemble tous les opérandes et opérateurs de la liste en une chaîne sans espaces
+        this.allNumber = this.liste.join("");
+        this.allNumber = this.allNumber.replace(/\s+/g, ""); // on retire les espaces induits par les opérateurs de la chaîne
     }
-  }
 
-  // Clear la liste et l'écran
-  reset() {
-    this.liste = [];
-    document.getElementById("result").innerHTML = "";
-    document.getElementById("calcul").innerHTML = "";
-  }
+    calc() { // effectue le calcul
+            this.joinList(); // on assemble la liste en une chaîne sans espaces
+            console.log(this.liste); // on affiche la liste à des fins de débogage
+            console.log(this.allNumber); // on affiche la chaîne de caractères à des fins de débogage
+            document.getElementById("result").innerHTML = this.secureEval(this.allNumber); // on affiche le résultat à l'écran
+            document.getElementById("calcul").innerHTML = this.allNumber; // on affiche l'opération complète qu'on vient de faire à l'écran
+            this.prec = []; // on supprime l'opération précédente
+            this.prec = this.liste; // on sauvegarde notre opération pour qu'elle devienne la précédente
+            this.liste = []; // on vide la liste pour pouvoir faire un nouveau calcul
+            this.end = performance.now(); // on arrête la mesure du temps
+            this.time = (this.end - this.start) / 1000;
+            // Affiche le temps d'opération dans la console, dans l'onglet et à l'écran
+            this.timeIcon + "\t" + this.time + "s";
+        }
+        // Supprime le dernier input
+    del() {
+        if (document.getElementById("result").innerHTML == "") {
+            // Si l'utilisateur appuie deux fois sur del après avoir fini un calcul
+            document.getElementById("result").innerHTML =
+                document.getElementById("calcul").innerHTML; // Le résultat précédent devient le résultat courant
+            document.getElementById("calcul").innerHTML = ""; // On efface l'opération précédente
+            this.liste = this.prec; // On remplace la liste actuelle avec l'opération précédente
+            this.start = performance.now(); // On reset le timer pour le nouveau calcul
+        } else {
+            // Sinon, on supprime juste le dernier caractère
+            this.liste.pop();
+            document.getElementById("result").innerHTML = this.liste.join("");
+        }
+    }
+
+
+
+    // Réinitialise la liste et l'écran
+    reset() {
+        this.liste = [];
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("calcul").innerHTML = "";
+    }
 }
-
-const calculator = new BaseCalculator();
+// Crée une instance de la classe BaseCalculator
+calculator = new BaseCalculator();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
+// Importation des modules nécessaires depuis NestJS
 import { Injectable } from '@nestjs/common';
 
+// Définition d'un service nommé "AppService"
 @Injectable()
 export class AppService {
+  // Définition d'une méthode qui renvoie une chaîne de caractères
   getHello(): string {
     return 'Hello World!';
   }
